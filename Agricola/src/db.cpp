46e8075b65a88466e90081ac9a11a0a24b3ac2a1
m1r <m1r@192.168.56.1>
2018-12-30 10:56:49 +0100

    ...

---
+++ Agricola/src/db.cpp
@@ -0,0 +1,21 @@
+/*
+ * db.cpp
+ *
+ *  Created on: 30 dic 2018
+ *      Author: m1r
+ */
+
+#include "db.h"
+
+namespace agr {
+
+Card1::Card1() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Card1::~Card1() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace agr */
---
+++ Agricola/src/db.h
@@ -0,0 +1,21 @@
+/*
+ * db.h
+ *
+ *  Created on: 30 dic 2018
+ *      Author: m1r
+ */
+
+#ifndef DB_H_
+#define DB_H_
+
+namespace agr {
+
+class Card1 {
+public:
+	Card1();
+	virtual ~Card1();
+};
+
+} /* namespace agr */
+#endif /* DB_H_ */
+
